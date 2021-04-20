b0b8c7e393c728ad7f490b5a7e9883e8ff0abf34
Elizaveta1903 <liza.liza19032001@gmail.com>
2021-04-20 11:50:05 +0700

    20.04

---
+++ asset/JS/main.js
@@ -0,0 +1,36 @@
+//alert("Hello Word!")
+
+/*
+	comment
+*/
+
+//создание переменных
+// 1. var глобальная
+// 2. let локальная
+
+var colorArray = [
+					"#5A9C6E", 
+					"#A8BF5A", 
+					"#CCC",
+					"#000", 
+					"#00FF00"
+					]
+var i = 0;
+function changeColor() {
+	document.body.style.background = colorArray[i];
+	
+	i++;
+
+	//console.log(colorArray[i])  показ кода 
+
+
+	if (i > colorArray.length -1) {
+		i = 0;
+	}
+}
+	function selectColor() {
+		let color = document.getElementById("color").value;
+		document.body.style.background = color;
+		console.log(color);
+
+	}
